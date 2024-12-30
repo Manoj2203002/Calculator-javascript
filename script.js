800f78a6c159ac59642f60adb9c26f2c5fd25490
Manoj2203002 <manoj220302@gmail.com>
2024-12-30 18:17:30 +0530

    New Commit

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const input=document.getElementById('Input');
-function Addvalue(value){
-    input.value=input.value+value;
-}
-function Delete(){
-    input.value='';
-}
-function calculate(){
-  if(input.value==''){
-    input.value="Undefined";
-}
-   if (/^[A-za-z]+$/.test(input.value))
-   {
-    input.value="error"
-   }
-else{
-    input.value=eval(input.value);
-}  
-}
-function erase(){
-    input.value=input.value.slice(0,-1)
-}
+    const input=document.getElementById('Input');
+    function Addvalue(value){
+        input.value=input.value+value;
+    }
+    function Delete(){
+        input.value='';
+    }
+    function calculate(){
+    if (/^[A-za-z]+$/.test(input.value))
+    {
+        input.value="error";
+    }
+    else{
+        input.value=eval(input.value);
+    }  
+    }
+    function erase(){
+        input.value=input.value.slice(0,-1)
+    }
